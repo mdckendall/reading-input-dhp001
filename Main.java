f9cc23efb194b26eac6cba1336758d86e10f94cf
Diego Hernandez <124943402+dhp001@users.noreply.github.com>
2023-02-28 16:24:50 +0000

    I returned length of string, not string itself.

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Enter a word:");
     
+    Scanner input= new Scanner(System.in);
+    String phrase= input.nextLine();
+    
+    System.out.println(phrase.length());
     //Your code will return the length of the entered String
   }
 }
